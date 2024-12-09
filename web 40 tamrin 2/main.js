de3782fac77e7ff1ab9ececa452b32441100398a
masoumehsanobari <masoumehsanobari@gmail.com>
2024-12-09 15:24:17 +0330

    Add files via upload

---
+++ web 40 tamrin 2/main.js
@@ -0,0 +1,38 @@
+let db =["m","a","s","o","u","m","e","h","s","a","n","o","b","a","r","i","1","3","8","6",]
+let a=["m","a","s","o","u","m","e","h","s","a","n","o","b","a","r","i","1","3","8","6",]
+let b=["m","a","s","o","u","m","e","h","s","a","n","o","b","a","r","i","1","3","8","6",]
+db.push("16")
+console.log(db)
+
+a.push("hi")
+console.log(a)
+
+b.push("how are you?")
+console.log(b)
+
+db.pop()
+console.log(db)
+
+a.pop()
+console.log(a)
+
+b.pop()
+console.log(b)
+
+db.shift()
+console.log(db)
+
+a.shift()
+console.log(a)
+
+b.shift()
+console.log(b)
+
+db.unshift("red")
+console.log(db)
+
+a.unshift("pink")
+console.log(a)
+
+b.unshift("blue")
+console.log(b)
